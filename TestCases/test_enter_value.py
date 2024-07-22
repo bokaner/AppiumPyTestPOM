f691c8c5b9425d56b46751c84c12667b2442bec6
bokaner <ujji.sharma@gmail.com>
2024-07-22 17:18:05 +0530

    added allure steps and allure description

--- TestCases/test_enter_value.py
+++ TestCases/test_enter_value.py
@@ -4,6 +4,7 @@ import pytest
 
 from Pages.AppiumDemo import AppiumDemo
 from TestCases.BaseTest import BaseTest
+from TestCases.conftest import allureLogs
 from Utilities import dataProvider
 import allure
 
@@ -22,15 +23,22 @@ class Test_Entervalue(BaseTest):
     #    appiumdemo.clickonEnterSomevalueButton()
     #      appiumdemo.entersomeValue(items)
 
+    @allure.description("this test case is to validate the text writtent on the Page")
     @allure.severity(allure.severity_level.MINOR)
     @pytest.mark.functional
     def test_validate_Text(self):
         self.ap = AppiumDemo(self.driver)
+        allureLogs("Checking the button is displayed ")
+
         displayed= self.ap.verify_entersome_button_isDisplayed()
+        #allureLogs("")
         print(displayed)
+        #allureLogs()
         self.ap.clickonEnterSomevalueButton()
+        allureLogs("button clicked successfully")
+
         title = self.ap.verify_the_title()
-        assert title == "Enter some "
+        assert title == "Enter some Values "
         print(title)
         self.ap.entersomeValue("ujjwal")
 
